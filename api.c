a73af7422fc5d6c598c7f307441a8c82cf384417
Lauri Gustafsson <luutifa@gmail.com>
2018-04-26 22:56:59 +0300

    Added more SDL API

--- api.c
+++ api.c
@@ -1,18 +1,33 @@
 #include <stdint.h>
 
 // SDL API
-typedef uint32_t Uint32;
-
-Uint32 SDL_INIT_VIDEO=0x00000020;
-Uint32 SDL_WINDOW_FULLSCREEN=0x00000001;
-Uint32 SDL_WINDOW_OPENGL=0x00000002;
+uint32_t SDL_INIT_VIDEO=0x00000020;
+uint32_t SDL_WINDOW_FULLSCREEN=0x00000001;
+uint32_t SDL_WINDOW_OPENGL=0x00000002;
+uint32_t SDL_QUIT=0x100;
+uint32_t SDL_KEYDOWN=0x300;
+int32_t SDLK_ESCAPE='\033';
 
 typedef struct SDL_Window SDL_Window;
 
-int SDL_Init(Uint32 flags);
-SDL_Window *SDL_CreateWindow(const char *title, int x, int y,
-        int w, int h, Uint32 flags);
-Uint32 SDL_GetTicks(void);
+typedef enum {
+    SDL_GL_CONTEXT_MAJOR_VERSION=17,
+    SDL_GL_CONTEXT_MINOR_VERSION=18,
+    SDL_GL_CONTEXT_PROFILE_MASK=21
+} SDL_GLattr;
+
+typedef union SDL_Event {
+    uint32_t type;
+    uint8_t padding[56];
+} SDL_Event;
+
+int SDL_Init(uint32_t flags);
+SDL_Window *SDL_CreateWindow(const char *title, int x, int y, int w, int h, uint32_t flags);
+uint32_t SDL_GetTicks(void);
+int SDL_GL_SetAttribute(SDL_GLattr attr, int value);
+void *SDL_GL_CreateContext(SDL_Window *window);
+void SDL_GL_SwapWindow(SDL_Window *window);
+int SDL_PollEvent(SDL_Event *event);
 
 // EGL API
 void *eglGetProcAddress(const char *procname);
--- main.c
+++ main.c
@@ -4,8 +4,25 @@
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindow("testi", 0, 0, 640, 480, SDL_WINDOW_OPENGL);
-    while (SDL_GetTicks() < 10000);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    // SDL_GL_CONTEXT_PROFILE_CORE == 0x0001
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, 1);
+
+    SDL_Window *w = SDL_CreateWindow("testi", 0, 0, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GL_CreateContext(w);
+
+    SDL_Event e;
+    while(1) {
+        SDL_GL_SwapWindow(w);
+
+        SDL_PollEvent(&e);
+        if (e.type == SDL_QUIT || e.type == SDL_KEYDOWN) {
+            break;
+        }
+    }
+
     return 0;
 }
 
