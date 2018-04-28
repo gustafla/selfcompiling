878add668c37cd58012966de483257042510b494
Lauri Gustafsson <luutifa@gmail.com>
2018-04-28 20:32:41 +0300

    Added more OpenGL API

--- api.c
+++ api.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stddef.h>
 
 // ------------------------------------ SDL API --------------------------------
 typedef struct SDL_Window W;
@@ -18,8 +19,12 @@ void *SDL_GL_GetProcAddress(const char*);
 int SDL_PollEvent(E*);
 
 // ------------------------------------ OpenGL API -----------------------------
+//
+// APIENTRY is not needed as this code is not meant for Winblows
+//
 // typedef int GLsizei;
 // typedef int GLint;
+// typedef ptrdiff_t GLsizeiptr;
 // typedef unsigned int GLenum;
 // typedef unsigned int GLuint;
 // typedef unsigned int GLbitfiled;
@@ -28,7 +33,19 @@ int SDL_PollEvent(E*);
 void glClearColor(float, float, float, float); // rgba
 void glClear(unsigned); // mask
 void glDrawArrays(unsigned, int, int); // mode, first, count
+
+// GetProcAddress calls
+// These could be implemented as short functions with custom names which call
+// the function directly from GetProcAddress. Consider it if size becomes scarce
+
 void (*glGenBuffers)(unsigned, unsigned*); // size, buffers
+void (*glBindBuffer)(unsigned, unsigned); // target, buffer
+void (*glBufferData)(unsigned, ptrdiff_t, const void*, unsigned);
 void gl() {
-    glGenBuffers=(void(*)(unsigned, unsigned*))SDL_GL_GetProcAddress("glGenBuffers");
+    glGenBuffers = (void(*)(unsigned, unsigned*))
+        SDL_GL_GetProcAddress("glGenBuffers");
+    glBindBuffer = (void(*)(unsigned, unsigned))
+        SDL_GL_GetProcAddress("glBindBuffer");
+    glBufferData = (void(*)(unsigned, ptrdiff_t, const void*, unsigned))
+        SDL_GL_GetProcAddress("glBufferData");
 }
