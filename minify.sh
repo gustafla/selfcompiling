246a91654fa73ad372abb10ecd1b9be65d8f3ab3
Lauri Gustafsson <luutifa@gmail.com>
2018-04-28 21:56:34 +0300

    Added comments to minify script

--- minify.sh
+++ minify.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+
+# remove extra spaces
 sed -rb 's/ {2,}//g' $1 |
+# remove line comments
 sed -rb 's/\/\/.*$//g' |
+# remove newlines
 tr -d '\n' |
+# remove comments
 sed -rb 's/\/\*.*\*\///g' |
+# add newline before #include
 sed -rb 's/#include/\n#include/g' |
+# add newline after >
 sed -rb 's/>/>\n/g'
+# ^ these work because C doesn't use <> for anything else than include
