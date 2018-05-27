f444b5ce2fe8e74a22374e38f7c4aee122baf944
Lauri Gustafsson <luutifa@gmail.com>
2018-05-27 13:51:08 +0300

    Better dev loop script

--- replol.sh
+++ replol.sh
@@ -2,10 +2,13 @@
 LOG=""
 
 while true; do
-    LOG=`make -B debug`
+    LOG=`make -B debug 2>&1 >/dev/null`
     ret=$?
-    if [ $ret -ne 0 ]; then echo $LOG | less; fi
-    LOG=`./demo`
-    echo $LOG | less
+    if [ $ret -ne 0 ]; then
+        echo $LOG | less
+    else
+        LOG=`./demo`
+        echo $LOG | less
+    fi
 done
 
