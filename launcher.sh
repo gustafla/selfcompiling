cab22e381f1d24e907b3a8fa40b397dff284cea0
Lauri Gustafsson <luutifa@gmail.com>
2018-06-01 13:16:03 +0300

    Wasted bytes to fancy output in launcher script

--- launcher.sh
+++ launcher.sh
@@ -1,1 +1,1 @@
-a="${TMPDIR:-/tmp}"/i.c;sed 1d $0|lzcat>$a;cc -O2 -lSDL2 -lGL -lm -o $a. $a;$a.;exit
+echo Decompressing...;a="${TMPDIR:-/tmp}"/i.c;sed 1d $0|lzcat>$a;echo Compiling...;cc -O2 -lSDL2 -lGL -lm -o $a. $a;echo Done.;$a.;exit
