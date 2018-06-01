79a5afc5a90fb9efc4efc87df19938e2f89e29b5
Lauri Gustafsson <luutifa@gmail.com>
2018-06-01 10:26:18 +0300

    shaders_to_c no longer minifies in debug mode.

--- shaders/shaders_to_c.sh
+++ shaders/shaders_to_c.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
-TERM=xterm
+TERM=xterm # Needed to avoid Mono bugs
 SOURCEFILES=`find * -name '*.frag' -o -name '*.vert'`
+OUTPUTFILE="../shaders.h.out"
 
-# Minify with Crtl-Alt-Test's tool
-for f in $SOURCEFILES; do
-    mono ~/misc/shader_minifier.exe --format none -o "$f.min" "$f"
-done
+# Minify with Crtl-Alt-Test's tool when needed
+if [ ! "$1" = "debug" ]; then
+    for f in $SOURCEFILES; do
+        mono ~/misc/shader_minifier.exe --format none -o "$f.min" "$f"
+    done
+fi
 
 # Convert to C source
-for f in *.min; do
-    f_clean=`basename $f .min`
-    F=`echo $f_clean | tr ./ _ | tr '[:lower:]' '[:upper:]'`
+for f in $SOURCEFILES; do
+    # Generate C identifiers/symbols for filenames
+    F=`echo $f | tr ./ _ | tr '[:lower:]' '[:upper:]'`
+
+    # Write C source code, only use minified when not debugging
     echo "char *$F=\"\\" > "$f.h.out"
     if [ "$1" = "debug" ]; then
-        echo "shaders/$f_clean\\" >> "$f.h.out"
+        echo "shaders/$f\\" >> "$f.h.out" # only write filename
     else
-        cat $f | sed 's/$/\\/' >> "$f.h.out"
+        cat "$f.min" | sed 's/$/\\/' >> "$f.h.out" # useless use of cat?
         echo >> "$f.h.out"
     fi
     echo "\";" >> "$f.h.out"
 done
 
-echo > ../shaders.h.out
+# Concatenate all shaders to a file
+echo > $OUTPUTFILE
 for f in *.h.out; do
-    cat $f >> ../shaders.h.out
+    cat $f >> $OUTPUTFILE
 done
-echo >> ../shaders.h.out
+echo >> $OUTPUTFILE
