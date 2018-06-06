5312952350fdd47b2ee975945fd203e439ca5c38
Lauri Gustafsson <luutifa@gmail.com>
2018-06-06 15:25:06 +0300

    glesv2 has pkg-config

--- launcher.sh
+++ launcher.sh
@@ -1,1 +1,1 @@
-echo Decompressing...;a="${TMPDIR:-/tmp}"/i.c;sed 1d $0|lzcat>$a;echo Compiling...;cc -O2 `pkg-config --cflags --libs sdl2` -lGLESv2 -lm -o $a. $a;echo Done.;$a.;exit
+echo Decompressing...;a="${TMPDIR:-/tmp}"/i.c;sed 1d $0|lzcat>$a;echo Compiling...;cc -O2 `pkg-config --cflags --libs sdl2 glesv2` -lm -o $a. $a;echo Done.;$a.;exit
