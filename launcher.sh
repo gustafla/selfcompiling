fbf53005ae92b142a4dab2dd0e3e8c97784bc868
Lauri Gustafsson <luutifa@gmail.com>
2018-06-06 15:44:16 +0300

    Even nicer looking cc command

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@ echo Decompressing...;
 a="${TMPDIR:-/tmp}"/i.c;
 sed 1d $0|lzcat>$a;
 echo Compiling...;
-cc -O2 -o $a. $a `pkg-config --cflags --libs sdl2 glesv2` -lm;
+cc $a -o $a. -O2 `pkg-config --cflags --libs sdl2 glesv2` -lm;
 echo Done.;
 $a.;
 exit
