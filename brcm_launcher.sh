8d1c708a7738844b02ab2e586d9729e40227e3d6
Lauri Gustafsson <luutifa@gmail.com>
2018-06-07 20:26:57 +0300

    brcm launcher fixes

--- brcm_launcher.sh
+++ brcm_launcher.sh
@@ -1,10 +1,8 @@
-PKG_CONFIG_PATH=/opt/vc/lib/pkgconfig;
-SDL_VIDEODRIVER=rpi;
 echo Decompressing...;
 a="${TMPDIR:-/tmp}"/i.c;
 sed 1d $0|lzcat>$a;
 echo Compiling...;
-cc $a -o $a. -O2 `pkg-config --cflags --libs sdl2 brcmglesv2` -lm;
+cc $a -o $a. -O2 `PKG_CONFIG_PATH=/opt/vc/lib/pkgconfig pkg-config --cflags --libs sdl2 brcmglesv2` -lm;
 echo Done.;
-$a.;
+SDL_VIDEODRIVER=rpi $a.;
 exit
