35b402e43d31477ae9ab4f638bac019c8b6ba8d6
Lauri Gustafsson <luutifa@gmail.com>
2018-07-27 22:34:22 +0300

    Shaping synth state etc

--- synth.c
+++ synth.c
@@ -3,9 +3,20 @@
 #endif
 
 typedef struct {
-    
+    size_t tick; // Current seq position
+    int speed; // Ticks per beat
+    int bps; // Beats per second
+    int out_channels; // Audio ouput channels
+    int out_rate; // Audio output sample rate
 } synth_t;
 
-void synth_generate_samples(synth_t *synth, float *buffer, size_t samples) {
+synth_t *synth_init(const char *track) {
+    synth_t *synth = malloc(sizeof(synth_t));
+    memset(synth, 0, sizeof(synth_t));
+
+    return synth;
+}
+
+void synth_render(synth_t *synth, float *buffer, size_t samples) {
     
 }
